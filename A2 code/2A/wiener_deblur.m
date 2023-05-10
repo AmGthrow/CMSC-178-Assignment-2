d7999b0a226e65622bd476de3308b5df22696c2a
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-05-10 21:37:52 +0800

    Add initial 2A solution

--- A2 code/2A/wiener_deblur.m
+++ A2 code/2A/wiener_deblur.m
@@ -10,14 +10,36 @@ Fi = fft2(I);
 
 % this section is just dummy code - delete it
 
-F_deblur = Fi;
-I_deblur = real( ifft2(F_deblur) );
+% F_deblur = Fi;
+% I_deblur = real( ifft2(F_deblur) );
 
 % Here you will need to:
+
 % 1. zero pad B and compute its FFT
+B = padarray(B, round((size(I) - size(B))/2), 0, "both");
+[HEIGHT, WIDTH] = size(I);
+B = B(1:HEIGHT, 1:WIDTH); % Trim off any rounding errors
+Fb = fft2(B);
+
 % 2. compute and apply the inverse filter
+Finv = zeros([HEIGHT WIDTH]);
+I_deblur = zeros([HEIGHT WIDTH]);
+for y=1:HEIGHT
+    for x=1:WIDTH
+        fi = Fi(y,x);
+        fb = Fb(y,x);
+        Finv(y, x) = (fi/fb)*((abs(fb)^2/(abs(fb)^2 + k)));
+        
+    end
+end
 % 3. convert back to a real image
+Inv = real(ifftshift(ifft2(Finv)));
 % 4. handle any spatial delay caused by zero padding of B
+for y=1:HEIGHT
+    for x=1:WIDTH
+        I_deblur(y,x) = Inv(y,x) * I(y,x);
+    end
+end
 %
 % you may need to deal with values near zero in the FFT of B etc
 % to avoid division by zero's etc.
