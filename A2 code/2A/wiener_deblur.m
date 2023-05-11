82c374333dde7556199593e2e06e8e85e429c474
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-05-11 09:28:24 +0800

    use correct deconvolution steps

--- A2 code/2A/wiener_deblur.m
+++ A2 code/2A/wiener_deblur.m
@@ -9,18 +9,19 @@ Fi = fft2(I);
 
 % modify the code below ------------------------------------------------
 
-% Zero pad B and compute its FFT
+% Zero pad B
 Bsize = size(B);
 B = padarray(B, fix((size(I) - Bsize)/2), 0, "pre");  % round down first half
 B = padarray(B, round((size(I) - Bsize)/2), 0, "post");  % round up second half
+% Convert B to frequency image
 Fb = fft2(B);
 
-% Compute inverse filter
-Finv = (Fi./Fb).*((abs(Fb).^2./(abs(Fb).^2 + k)));
-% Convert inverse to real image
-Inv = real(ifftshift(ifft2(Finv)));
-% Apply inverse filter
-I_deblur = Inv .* I;
+% Compute and apply inverse filter
+Fb2 = abs(Fb).^2;
+F_deblur = (Fi ./ Fb) .* ((Fb2 ./ (Fb2 + k)));
+
+% Convert deblur to real image
+I_deblur = ifftshift(real(ifft2(F_deblur)));
 
 % modify the code above ------------------------------------------------
 
