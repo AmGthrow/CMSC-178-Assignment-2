6c59d1bdc9d87347aedbccbbac7514d0a5bd1d86
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2023-05-12 16:10:07 +0800

    Avoid major changes to deconv_test2

--- A2 code/2A/deconv_test2.m
+++ A2 code/2A/deconv_test2.m
@@ -2,11 +2,11 @@
 % DETERMINE the best K to be able to read the number plates
 %
 
-NOISE = input('Enter an estimate for noise:');
-k=input('Enter an estimate for k (1 .. 0.00001):');
-B=fspecial('gaussian',65,NOISE);
+B=fspecial('gaussian',65,10);
 
 I = double(imread('example3.png'))/255; I=I(:,:,1);
+
+k=input('Enter an estimate for k (1 .. 0.00001):');
 
 Iinv = wiener_deblur(I,B,k);
 
@@ -20,5 +20,5 @@ imagesc(B); axis equal tight;
 title('PSF (Guess)');
 subplot(1,5,[4 5]);
 imagesc(Iinv); axis equal tight; caxis([0 1]);
-title(['Deconvolved Image (K=' num2str(k) ', noise=' num2str(NOISE) ')']);
+title(['Deconvolved Image (K=' num2str(k) ')']);
 drawnow;
